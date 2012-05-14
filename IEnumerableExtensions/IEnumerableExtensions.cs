4a5dcf594da9d97ac72e78d0068901fe0475c4d3
John Sietsma <john.sietsma@gmail.com>
2012-05-14 15:39:10 +1000

    Added ForEach extension.

---
+++ IEnumerableExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+public static class IEnumerableExtensions
+{
+    public static void ForEach<T>( this IEnumerable<T> enumeration, Action<T> action )
+    {
+        foreach( T item in enumeration ) {
+            action( item );
+        }
+    }
+}
+
