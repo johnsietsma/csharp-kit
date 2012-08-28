c92b100ee65f57e8b1e1193a8173707cef503447
John Sietsma <john.sietsma@gmail.com>
2012-08-28 16:41:34 +1000

    Minor tidy up. Remove commented code, Remove test binaries.

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -2,11 +2,6 @@ using System.Collections.Generic;
 
 public static class ListExtensions
 {
-    /*public static string ToStringJoin<T> (this List<T> list)
-    {
-        return list.ToStringJoin<T>();
-    }*/
-
     public static void Remove<T> (this List<T> list, IEnumerable<T> toRemove)
     {
         if (list == null || toRemove == null)
