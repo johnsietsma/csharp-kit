8e0c478dc30c8ae5dd73b869b500936c8638fa3c
John Sietsma <john.sietsma@gmail.com>
2012-09-19 22:06:24 +1000

    - Added Zip to IEnumerable<T> extensions.

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -27,5 +27,18 @@ public static class IEnumerableExtensions
         items.ForEach( c=>ret+=p(c));
         return ret;
     }
+
+    public static IEnumerable<T> Zip<A, B, T>(
+        this IEnumerable<A> seqA, IEnumerable<B> seqB, Func<A, B, T> func)
+    {
+        using (var iteratorA = seqA.GetEnumerator())
+            using (var iteratorB = seqB.GetEnumerator())
+        {
+            while (iteratorA.MoveNext() && iteratorB.MoveNext())
+            {
+                yield return func(iteratorA.Current, iteratorB.Current);
+            }
+        }
+    }
 }
 
