54dbd36ca7b76bf33aeecdd0e466736df83677a0
Rhys van der Waerden <rhys.vdw@gmail.com>
2013-02-13 11:37:31 +1100

    Enums.Get returns an array instead of an IEnumerable

--- Enums/Enums.cs
+++ Enums/Enums.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 public static class Enums {
-    public static IEnumerable<T> Get<T>()
+    public static T[] Get<T>()
     {
-        List<T> values = ((T[])Enum.GetValues( typeof(T) )).ToList();
-        return values;
+        return (T[]) Enum.GetValues( typeof(T) );
     }
 }
