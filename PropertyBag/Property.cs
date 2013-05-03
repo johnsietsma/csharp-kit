22d530732302b0bff9cb245c6908aedb12190699
John Sietsma <john.sietsma@gmail.com>
2013-05-03 10:13:23 +1000

    Added PropertyBag.

---
+++ PropertyBag/Property.cs
@@ -0,0 +1,77 @@
+using System;
+
+/// <summary>
+/// A single property
+/// </summary>
+[Serializable()]
+public class Property : System.Object
+{
+    #region Member Variables
+    private string _propName;
+    private System.Object _value;
+    private System.Object _owner;
+
+    #endregion
+
+    #region Constructor(s)
+    public Property( string name, System.Object owner )
+    {
+        _propName = name;
+        _owner = owner;
+        _value = null;
+    }
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Gets/Sets the value of this Property
+    /// </summary>
+    public System.Object Value
+    {
+        get {
+            lock ( this ) {
+                System.Reflection.PropertyInfo propInfo = Owner.GetType().GetProperty( Name );
+                if ( null != propInfo ) {
+                    return propInfo.GetValue( Owner, new object[] { } );
+                }
+                else {
+                    return _value;
+                }
+            }
+        }
+        set {
+            lock ( this ) {
+                System.Reflection.PropertyInfo propInfo = Owner.GetType().GetProperty( Name );
+
+                if ( null != propInfo ) {
+                    propInfo.SetValue( Owner, value, new object[] { } );
+                }
+                else {
+                    _value = value;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets the owner of this property
+    /// </summary>
+    public System.Object Owner
+    {
+        get {
+            return _owner;
+        }
+    }
+
+    /// <summary>
+    /// Gets the friendly name of the Property
+    /// </summary>
+    public string Name
+    {
+        get {
+            return _propName;
+        }
+    }
+
+    #endregion
+}
---
+++ PropertyBag/PropertyBag.cs
@@ -0,0 +1,64 @@
+// originally from http://rogers-neighborhood.com/wordpress/?p=92
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class that represents the "bag" o' properties
+/// </summary>
+[Serializable()]
+public class PropertyBag : System.Object
+{
+    #region Member Variables
+    private Dictionary<string,Property> _propCollection = new Dictionary<string,Property>();
+    #endregion
+
+    #region Constructor(s)
+    public PropertyBag() 
+    {
+        Owner = this;
+    }
+
+    public PropertyBag( System.Object owner )
+    {
+        Owner = owner;
+    }
+    #endregion
+
+    /// <summary>
+    /// Gets the owner of this property bag.
+    /// </summary>
+    public System.Object Owner { get;  private set; }
+
+    /// <summary>
+    /// The 'this' operator that reuturn a Property type.
+    /// </summary>
+    ///
+    ///
+    public Property this[string Name]
+    {
+        get {
+            Property prop;
+            if ( _propCollection.ContainsKey( Name ) ) {
+                prop = ( Property )_propCollection[Name];
+            }
+            else {
+                prop = new Property( Name, Owner );
+                _propCollection.Add( Name, prop );
+            }
+            return prop;
+        }
+    }
+
+    public T GetValue<T>() {
+        return GetValue<T>( typeof(T).Name );
+    }
+
+    public T GetValue<T>( string name ) {
+        if( !_propCollection.ContainsKey(name) ) {
+            return default(T);
+        }
+        
+        Property p = this[name];
+        return (T)p.Value;
+    }
+}
